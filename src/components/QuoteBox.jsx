629e8688f28b4e3df379b7538741bc91786aed85
JCCRUZCHACON <confeccionesmc2018@gmail.com>
2023-07-30 10:54:21 -0500

    PROTECTO1.1

---
+++ src/components/QuoteBox.jsx
@@ -0,0 +1,21 @@
+import './styles/QuoteBox.css';
+
+const QuoteBox = ({handleChangeQuote, quote}) => {
+    
+  return (
+    <section className='quoteBox'>
+        <h1>Fortuna Cookies</h1>
+        <section>
+            <button className="button" onClick={handleChangeQuote}>TRY LUCK</button>
+            <article>
+                <p>
+                    {quote.phrase}
+                </p>
+            </article>
+        </section>
+        <footer><h4>autor: {quote.author}</h4></footer>
+        <article className='git'><b>JUAN CARLOS CRUZ CHACON <a href="https://github.com/JCCRUZCHACON/PROYECT_G28/">GITHUB</a> </b></article>
+    </section>
+  )
+}
+export default QuoteBox
