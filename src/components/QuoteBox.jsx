89c9b4d4b275f43fbe7aa5d431d477a95c432ebe
JCCRUZCHACON <confeccionesmc2018@gmail.com>
2023-07-31 01:38:24 -0500

    PROCAM1

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,20 +1,24 @@
-import './styles/QuoteBox.css';
+import "./styles/QuoteBox.css";
 
-const QuoteBox = ({handleChangeQuote, quote}) => {
-    
+const QuoteBox = ({ handleChangeQuote, quote }) => {
   return (
-    <section className='quoteBox'>
-        <h1>Fortuna Cookies</h1>
-        <section>
-            <button className="button" onClick={handleChangeQuote}>TRY LUCK</button>
-            <article>
-                <p>
-                    {quote.phrase}
-                </p>
-            </article>
-        </section>
-        <footer><h4>autor: {quote.author}</h4></footer>
+    <section className="quoteBox">
+      <h1>Fortuna Cookies</h1>
+      <section>
+        <div className="but">
+          <button className="button" onClick={handleChangeQuote}>
+            TRY LUCK
+          </button>
+        </div>
+
+        <article>
+          <p>{quote.phrase}</p>
+        </article>
+      </section>
+      <footer>
+        <h4>autor: {quote.author}</h4>
+      </footer>
     </section>
-  )
-}
-export default QuoteBox
+  );
+};
+export default QuoteBox;
